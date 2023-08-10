cdad75a6152d07424239aac6fd3cf6676b05da67
ravibootlabs <ravishankar@Ravi-MacBook-Air.local>
2023-08-10 13:53:27 +0530

    terraform fmt

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
-resource "azurerm_container_group" "example"{
-name = var.name
-resource_group_name = var.resource_group_name
-location = var.location
-os_type = var.os_type
-ip_address_type = var.ip_address_type
-container {
-name = var.container_name
-image = var.image
-cpu = var.cpu
-memory = var.memory
- ports {
-   port = var.port
-   protocol = var.protocol
+resource "azurerm_container_group" "example" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  os_type             = var.os_type
+  ip_address_type     = var.ip_address_type
+  container {
+    name   = var.container_name
+    image  = var.image
+    cpu    = var.cpu
+    memory = var.memory
+    ports {
+      port     = var.port
+      protocol = var.protocol
+    }
   }
- }
 }
